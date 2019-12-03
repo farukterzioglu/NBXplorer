0b90c8861e9586824eafb358d1be7b55e1dd3f8d
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2019-12-03 20:52:57 +0300

    appveyor

--- run-docker.cmd
+++ run-docker.cmd
@@ -1,3 +1,4 @@
+C:\Program Files\Docker\docker.exe -SwitchLinuxEngine
 docker-compose -f docker-compose.regtest.yml down
 docker-compose -f docker-compose.regtest.yml up --force-recreate --build
 pause
